01265fa1fcd2a28c6c19540466946e820615a050
evgeniy_MacBook <pavlinbl4@gmail.com>
2025-02-21 15:43:30 +0300

    убрал мусор и проставил цифры у handlers

--- main.py
+++ main.py
@@ -33,6 +33,7 @@ storage = MemoryStorage()
 bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher(storage=storage)
 
+
 def is_allowed_file_type(mime_type: str) -> bool:
     allowed_files_type = {'image/jpeg', 'image/png', 'image/x-tiff'}
     return mime_type in allowed_files_type
@@ -51,14 +52,9 @@ def convert_to_jpeg_if_needed(file_path: str) -> str:
 class FSMFillForm(StatesGroup):
     add_file = State()  # Состояние ожидания добавления файла
     add_credit = State()  # Состояние ожидания ввода image credit
-    # add_caption = State()  # Состояние ожидания выбора image caption
 
 
-# async def set_main_menu(bot: Bot):
-#     await bot.set_my_commands(commands=kp_uploader)
-
-
-# handler будет срабатывать на команду /start вне состояний
+# handler_01 будет срабатывать на команду /start вне состояний
 # и предлагать отправить фото, отправив команду /add_image
 @dp.message(CommandStart(), StateFilter(default_state))
 async def process_start_command(message: Message):
@@ -70,6 +66,7 @@ async def process_start_command(message: Message):
     logger.info("бот стартовал")
 
 
+# handler_02_1 будет срабатывать на команду "/help"
 @dp.message(Command(commands='help'), StateFilter(default_state))
 async def process_start_command(message: Message):
     await message.answer(
@@ -80,7 +77,7 @@ async def process_start_command(message: Message):
     )
 
 
-# handler будет срабатывать на команду "/cancel" в любых состояниях,
+# handler_02_2 будет срабатывать на команду "/cancel" в любых состояниях,
 @dp.message(Command(commands='cancel'))
 async def process_cancel_command_state(message: Message, state: FSMContext):
     await message.answer(
@@ -93,7 +90,7 @@ async def process_cancel_command_state(message: Message, state: FSMContext):
 
 
 # handler_03 будет срабатывать на команду /add_image
-# в новой версии 3 хэндлера вначале хочу получить данные о правообладателе
+# в новой версии вначале хочу получить данные о правообладателе
 
 @dp.message(Command(commands='add_image'), StateFilter(default_state), F.from_user.username.in_(ALLOWED_USER_NAMES))
 async def process_add_image_command(message: Message, state: FSMContext):
@@ -101,8 +98,6 @@ async def process_add_image_command(message: Message, state: FSMContext):
     logger.info("Запрос кредитов на снимок")
     # Устанавливаем состояние ожидания ввода кредита
     await state.set_state(FSMFillForm.add_credit)
-
-
 
 
 async def process_single_file(uploaded_file: types.Document, message: types.Message, state: FSMContext) -> None:
@@ -126,7 +121,7 @@ async def process_single_file(uploaded_file: types.Document, message: types.Mess
 
         # добавляю фото в фотоархив
         data = await state.get_data()
-        photo_id = 'test_photo_id_no_real_upload'
+
         photo_id = web_photo_uploader(data["path_to_uploaded_image"], data["image_file_name"], data["credit"])
 
         logger.info(f"id снимка получено - {photo_id}")
@@ -137,19 +132,18 @@ async def process_single_file(uploaded_file: types.Document, message: types.Mess
         await message.answer(
             f"Произошла ошибка при обработке файла {uploaded_file.file_name}. Пожалуйста, попробуйте еще раз.")
 
+
 # handler_04 будет срабатывать, если отправлено фото
 # и переводить в состояние ожидания ввода автора фото
 @dp.message(StateFilter(FSMFillForm.add_file))
 async def handle_allowed_user_messages(message: types.Message, state: FSMContext):
     logger.info(message.document)
-
-    # если message.document is None - значит прислали не файл и не группу файлов
+    # если message.document is None - значит прислали не файл и не медиа группу
     if message.document is None:
         await message.answer(f"Отправьте фото «как файл», чтоб сохранить качество\n"
                              f"снимка")
         await state.set_state(FSMFillForm.add_file)
 
-    #
     else:
         uploaded_files = message.document if isinstance(message.document, list) else [message.document]
         logger.info(f'{uploaded_files = }')
@@ -163,9 +157,7 @@ async def handle_allowed_user_messages(message: types.Message, state: FSMContext
                 await state.set_state(FSMFillForm.add_file)
 
 
-
-
-# handler будет срабатывать, если введен корректный кредит
+# handler_05 будет срабатывать, если введен корректный кредит
 # и переводить в состояние ожидания добавления снимка/снимков
 @dp.message(StateFilter(FSMFillForm.add_credit), F.text.len() > 3)
 async def process_name_sent(message: Message, state: FSMContext):
@@ -174,31 +166,7 @@ async def process_name_sent(message: Message, state: FSMContext):
     await message.answer(text='Спасибо!\n\nА теперь загрузите снимки как файл')
     await state.set_state(FSMFillForm.add_file)
 
-
-# handler будет срабатывать, если введено корректное имя
-# и переводить в состояние ожидания ввода описания
-# @dp.message(StateFilter(FSMFillForm.add_caption), F.text.len() > 3)
-# async def process_caption_sent(message: Message, state: FSMContext):
-#     # сохраняем введенное имя в хранилище по ключу "caption"
-#     await state.update_data(caption=f"{message.document} - {message.text}")
-#
-#     data = await state.get_data()
-#     # await message.answer(f'{data["caption"]}')
-#     await message.answer(text='Спасибо!\n\nВ ближайшее время вам поступит id снимка')
-#     # Устанавливаем состояние ожидания ввода описания
-#
-#     # await state.set_state(FSMFillForm.add_caption)
-#     await state.clear()
-#     # ic(f'{data["caption"]}\n{data["credit"]}')
-#     logger.info(f'{data["caption"]}\n{data["credit"]}')
-#     # photo_id = "test photo id"
-#     # добавляю фото в фотоархив
-#     photo_id = web_photo_uploader(data["path_to_uploaded_image"], data["caption"], data["credit"])
-#
-#     logger.info(f"id снимка получено - {photo_id}")
-#     await message.answer(text=f'Готово!\n\n{photo_id = }')
-
-
+# handler_06
 @dp.message(Command(commands='add_image'), StateFilter(default_state))
 async def handle_other_messages(message: types.Message):
     # This function will be called for messages from any other user
@@ -209,7 +177,7 @@ async def handle_other_messages(message: types.Message):
                          f"список пользователей .")
 
 
-# handler будет срабатывать, если введено корректное автора/правообладателя
+# handler_07 будет срабатывать, если введено корректное автора/правообладателя
 # и переводить в состояние ожидания добавления файла
 @dp.message(StateFilter(FSMFillForm.add_credit), F.text.len() < 3)
 async def process_credit_sent(message: Message, state: FSMContext):
@@ -219,7 +187,7 @@ async def process_credit_sent(message: Message, state: FSMContext):
     # Устанавливаем состояние ожидания добавления файла
     await state.set_state(FSMFillForm.add_file)
 
-
+# handler_08
 @dp.message(StateFilter(default_state))
 async def handle_other_messages_2(message: types.Message):
     # This function will be called for messages from any other user
